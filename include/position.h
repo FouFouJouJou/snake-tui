ae59af2fb7109a5a2514c1541a2b8035162a1946
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-06 20:13:33 +0100

    position api implemented

--- include/position.h
+++ include/position.h
@@ -5,5 +5,5 @@
 struct Position {
   uint32_t x,y;
 };
-
+struct Position make_position(uint32_t x, uint32_t y);
 #endif
---
+++ source/position.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <position.h>
+struct Position make_position(uint32_t x, uint32_t y) {
+  struct Position pos={.x=x, .y=y};
+  return pos;
+}
