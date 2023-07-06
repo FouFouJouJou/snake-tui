0e1dadc4db0a729de7eecbad32b22af930d5cdf9
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-06 18:07:23 +0100

    added list api prototypes

--- include/list.h
+++ include/list.h
@@ -1,1 +1,23 @@
+#ifndef __LIST_H__  
+#define __LIST_H__
+struct Node {
+  void *value;  
+  struct Node *next;
+};
 
+struct List {
+  struct Node *head;
+  struct Node *tail;
+};
+
+struct Node *make_empty_node();
+struct Node *make_node(void *value, size_t size);
+void free_node(struct Node *node);
+struct List *make_empty_list();
+void push(struct List *list, struct Node *node);
+void append(struct List *list, struct Node *node);
+struct Node *pop_end(struct List *list);
+void free_list(struct List *list);
+void printf_list(struct List list);
+void printf_node(struct Node node);
+#endif
