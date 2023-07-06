58cb39db7774682ddc8036bf828edbd2edaf04f0
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-06 18:46:30 +0100

    testing list api

---
+++ test/list_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <list.h>
+
+void printf_int_node(struct Node node) {
+  printf("%d -> ", *((int *)(node.value)));
+}
+
+int main(void) {
+  struct List *list=make_empty_list();
+  int int1=1, int3=3, int0=0;
+  append(list, make_node(&int1, sizeof(int)));
+  append(list, make_node(&int3, sizeof(int)));
+  push(list, make_node(&int0, sizeof(int)));
+  printf_list(*list, printf_int_node);
+  free_list(list); 
+  return EXIT_SUCCESS;
+}
