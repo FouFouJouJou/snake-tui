416047191531107afa99d87ecd21d30b48b0d0f1
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-07 01:12:52 +0100

    adding random position generator routine

--- source/position.c
+++ source/position.c
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include <position.h>
+
 struct Position make_position(uint32_t x, uint32_t y) {
   struct Position pos={.x=x, .y=y};
   return pos;
+}
+struct Position make_random_position_in_range(uint32_t x_limit, uint32_t y_limit) {
+  return make_position(x_limit, y_limit);
 }
