e106e8dd0fa498f7e2d3e5fa489c6423fa58fd8f
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-07 23:30:53 +0100

    adding asserts to fix bugs

--- source/food.c
+++ source/food.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 #include <time.h>
 #include <food.h>
 #include <position.h>
@@ -14,7 +15,9 @@ struct Food make_food(uint32_t x, uint32_t y, char shape) {
 
 struct Food make_random_food(uint32_t x_limit, uint32_t y_limit, char shape) {
   srand(time(0));
-  uint32_t random_x=(rand()%(x_limit))+1;
-  uint32_t random_y=(rand()%(y_limit))+1;
+  uint32_t random_x=(rand()%(x_limit));
+  uint32_t random_y=(rand()%(y_limit));
+  assert(random_x < x_limit);
+  assert(random_y < y_limit);
   return make_food(random_x, random_y, shape);
 }
