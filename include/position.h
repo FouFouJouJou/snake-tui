6d5cae86d4df87d83aeb293c86eac5210edaca5d
firasjaballiftw <firasjaballiftw@gmail.com>
2023-07-07 22:44:41 +0100

    changing position api

--- include/position.h
+++ include/position.h
@@ -6,5 +6,4 @@ struct Position {
   uint32_t x,y;
 };
 struct Position make_position(uint32_t x, uint32_t y);
-struct Position make_random_position_in_range(uint32_t x_limit, uint32_t y_limit);
 #endif
--- source/position.c
+++ source/position.c
@@ -5,6 +5,3 @@ struct Position make_position(uint32_t x, uint32_t y) {
   struct Position pos={.x=x, .y=y};
   return pos;
 }
-struct Position make_random_position_in_range(uint32_t x_limit, uint32_t y_limit) {
-  return make_position(x_limit, y_limit);
-}
